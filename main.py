b391369e9eaac533f8e3b9247726fee65ed9277c
honkai-yan <2718504358@qq.com>
2024-10-05 22:46:23 +0800

    add: 完工

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, csv, requests
+import sys, csv, requests, os
 from bs4 import BeautifulSoup as bs
 sys.path.append("./")
 from utils import tupleEquals
@@ -7,6 +7,8 @@ targetUrl = f"https://detail.zol.com.cn/cell_phone_index/subcate57_list_1.html"
 header = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
 }
+userPath = os.path.expanduser("~")
+fileName = "phoneRank.csv"
 
 
 def main():
@@ -20,7 +22,7 @@ def main():
     if item.name == "li":
       phoneList.append(item)
   dataList = savePhoneData(phoneList)
-  print(len(dataList))
+  writeData(userPath, dataList)
 
 
 def findTargetdElm(tagType: str, className: tuple, bsObj: bs):
@@ -42,23 +44,38 @@ def savePhoneData(phoneList: list = []) -> list:
   dataList = []
   for data in phoneList:
     h3 = data.h3
-    phoneData = {}
+    phoneData = []
     if h3:
-      phoneData["phoneName"] = h3.a["title"]
+      phoneData.append(h3.a["title"])
     else:
       continue
 
     priceText = findTargetdElm("b", ["price-type"], data)
     if priceText:
-      phoneData["price"] = priceText.string
+      phoneData.append(priceText.string)
 
     scoreText = findTargetdElm("span", ["score"], data)
     if scoreText:
-      phoneData["score"] = scoreText.string
+      phoneData.append(scoreText.string)
     
     dataList.append(phoneData)
   
   return dataList
+
+
+def writeData(userPath: str, dataList: list):
+  filePath = os.path.join(userPath, fileName)
+  with open(filePath, mode="w", newline="") as file:
+    csvWriter = csv.writer(file)
+    csvWriter.writerows([
+      [
+        "手机型号",
+        "手机价格",
+        "手机评分"
+      ]
+    ])
+    for data in dataList:
+      csvWriter.writerows([data])
 
 
 def failAndExit(msg="爬取网页失败"):
